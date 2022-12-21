9663a66df698c33b9d9ec5c69272a8100ef8d8be
yoshivn1203 <yoshivn1203@gmail.com>
2022-12-21 14:09:51 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const carousel = document.querySelector(".product-carousel");
+const itemsContainer = carousel.querySelector(".product-carousel-items");
+const items = Array.from(itemsContainer.children);
+const prevButton = carousel.querySelector(".carousel-button-prev");
+const nextButton = carousel.querySelector(".carousel-button-next");
+
+let currentItemIndex = 0;
+let itemsPerPage = 5.5;
+
+function goToItem(index) {
+  if (index < 0) {
+    index = items.length - itemsPerPage;
+  } else if (index > items.length - itemsPerPage) {
+    index = 0;
+  }
+  itemsContainer.style.transition = "transform 0.5s ease";
+  itemsContainer.style.transform = `translateX(-${
+    index * (100 / itemsPerPage)
+  }%)`;
+  currentItemIndex = index;
+}
+
+prevButton.addEventListener("click", () => {
+  goToItem(currentItemIndex - 1);
+});
+
+nextButton.addEventListener("click", () => {
+  goToItem(currentItemIndex + 1);
+});
+
+goToItem(currentItemIndex);
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 800) {
+    itemsPerPage = 2;
+  } else {
+    itemsPerPage = 6;
+  }
+  goToItem(currentItemIndex);
+});
